59fd73189b4f987b9e01377d0271dd0f85d23a69
IvanSlivenko <ivan8822@ukr.net>
2025-07-27 13:04:22 +0300

    27-07-2025-13-04

--- project/example_3/src/index.ts
+++ project/example_3/src/index.ts
@@ -5,6 +5,18 @@ const app = express();
 app.use(cors());
 const port = 3000;
 
+const jsonBodyMiddleware = express.json()
+app.use(jsonBodyMiddleware)
+
+const db = {
+  courses: [
+    { id: 1, title: "front-end" },
+    { id: 2, title: "back-end" },
+    { id: 3, title: "automation ga" },
+    { id: 4, title: "devops" },
+  ],
+};
+
 // app.get('/', (req, res) => {
 //   // const a = 4;
 //   // if (a > 5) {
@@ -29,17 +41,17 @@ const port = 3000;
 //   res.send('We hav created samurai method POST !!')
 // })
 
-const db = {
-  courses: [
-    { id: 1, title: "front-end" },
-    { id: 2, title: "back-end" },
-    { id: 3, title: "automation ga" },
-    { id: 4, title: "devops" },
-  ],
-};
+// app.get("/courses", (req, res) => {
+//   res.json(db.courses);
+// });
 
 app.get("/courses", (req, res) => {
-  res.json(db.courses);
+  
+  let foundCourses = db.courses
+  if(req.query.title){
+    foundCourses = foundCourses.filter(c=> c.title.indexOf(req.query.title as string)  > -1 )
+  }
+  res.json(foundCourses);
 });
 
 app.get("/courses/:id", (req, res) => {
@@ -52,6 +64,16 @@ app.get("/courses/:id", (req, res) => {
 
   res.json(foundCourse);
 });
+
+app.post("/courses", (req, res)=>{
+  const createdCourse = {
+    id: +(new Date()),
+    title: req.body.title
+  }
+  db.courses.push(createdCourse)
+  res.json(createdCourse)
+
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
